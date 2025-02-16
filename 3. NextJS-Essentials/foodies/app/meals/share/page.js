eb66b302e4ef022311e90a7a5c288e7f757bac10
RahulPrasad13400 <rahulprasad1399@gmail.com>
2025-02-16 19:41:50 +0530

    form submission

--- 3. NextJS-Essentials/foodies/app/meals/share/page.js
+++ 3. NextJS-Essentials/foodies/app/meals/share/page.js
@@ -1,7 +1,21 @@
-import ImagePicker from '@/app/meals/image-picker'
+import ImagePicker from '@/app/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  async function shareMeal(formData) {
+    'use server';
+
+    const meal = {
+      title: formData.get('title'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email')
+    }
+
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -10,8 +24,8 @@ export default function ShareMealPage() {
         </h1>
         <p>Or any other meal you feel needs sharing!</p>
       </header>
-      <main className={classes.main}>   
-        <form className={classes.form}>
+      <main className={classes.main}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +53,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
